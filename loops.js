bcd15dbb3ab137b1a08b5deee62686bdd09935e1
Berkay Akcakil <burkeakcakil@gmail.com>
2018-11-17 04:35:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-function forLoop(arrayIn){
+function forLoop(array){
   for (let i = 0; i < 25; i++) {
     if(i==1){
-      arrayIn.push("I am 1 strange loop.")
+      array.push("I am 1 strange loop.")
     } else {
-      arrayIn.push("I am " + i + " strange loops.")
+      array.push("I am " + i + " strange loops.")
   }
 }
-return arrayIn
+return array
 }
